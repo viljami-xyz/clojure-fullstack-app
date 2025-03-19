4fc912962901eeafaba1f2f57dca97bd59f9d4fc
Viljami Kilkkilä <viljami.kilkkila@gmail.com>
2025-03-19 05:32:49 +0200

    The default setup of Clojure project

---
+++ backend/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jagers/backend
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ backend/test/jagers/backend_test.clj
@@ -0,0 +1,7 @@
+(ns jagers.backend-test
+  (:require [clojure.test :refer :all]
+            [jagers.backend :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
