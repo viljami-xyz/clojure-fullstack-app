b15586bbd0d9abef2ddb197e151fba9d4ce7447e
Viljami Kilkkilä <viljami.kilkkila@gmail.com>
2025-04-01 18:56:03 +0300

    Testing utils lib

--- worker/project.clj
+++ worker/project.clj
@@ -3,8 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [utils "0.1.0-SNAPSHOT"]
+                 ]
   :main ^:skip-aot worker.core
   :target-path "target/%s"
+  :source-paths ["src" "../utils/src"]
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- worker/src/worker/core.clj
+++ worker/src/worker/core.clj
@@ -1,7 +1,10 @@
 (ns worker.core
+  (:require [utils.core :as utils])
   (:gen-class))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (utils/create-table)
   (println "Hello, World!"))
